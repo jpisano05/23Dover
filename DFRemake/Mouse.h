b9cfdd81652f3cf4ef95c68556d29521319cb6ec
Boommook <nmbennett@wpi.edu>
2025-02-28 16:14:48 -0500

    Added incomplete game and mouse files

---
+++ DFRemake/Mouse.h
@@ -0,0 +1,14 @@
+// Mouse Header
+
+#include "Object.h"
+#include "EventCollision.h"
+
+class Mouse : public df : Object{
+private:
+	void out();
+	void step();
+public:
+	Mouse();
+	~Mouse();
+	int eventHandler(const df::Event* p_e) override;
+}
