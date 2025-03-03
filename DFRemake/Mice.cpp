fb0d8d6c2e62d81c415ffe928ef4cda0276368e3
Boommook <nmbennett@wpi.edu>
2025-03-03 17:07:46 -0500

    fixed path, added new harder path, edited mice, edited animation

--- DFRemake/Mice.cpp
+++ DFRemake/Mice.cpp
@@ -19,6 +19,8 @@ Mice::Mice() {
 	// set starting velocity
 	setVelocity({1, 0 });
 
+	setAltitude(2);
+
 	curr_path = 1;
 }
 
@@ -47,31 +49,31 @@ void Mice::step() {
 
 	// check for change of direction
 
-	if (curr_path == 1 && getPosition().getX() >= 75) {
+	if (curr_path == 1 && getPosition().getX() >= 74.5) {
 		setSprite("mouse-down");
 		setVelocity({ 0, 1});
 		curr_path++;
 	}
 
-	else if (curr_path == 2 && getPosition().getY() >= 13) {
+	else if (curr_path == 2 && getPosition().getY() >= 12) {
 		setSprite("mouse-left");
 		setVelocity({-1, 0 });
 		curr_path++;
 	}
 
-	else if (curr_path == 3 && getPosition().getX() <= 45) {
+	else if (curr_path == 3 && getPosition().getX() <= 45.5) {
 		setSprite("mouse-up");
 		setVelocity({0, -1});
 		curr_path++;
 	}
 
-	else if (curr_path == 4 && getPosition().getY() <= 6) {
+	else if (curr_path == 4 && getPosition().getY() <= 5) {
 		setSprite("mouse-left");
 		setVelocity({ -1, 0 });
 		curr_path++;
 	}
 
-	else if (curr_path == 5 && getPosition().getX() <= 5) {
+	else if (curr_path == 5 && getPosition().getX() <= 5.5) {
 		setSprite("mouse-down");
 		setVelocity({0, 1 });
 		curr_path++;
@@ -83,19 +85,19 @@ void Mice::step() {
 		curr_path++;
 	}
 
-	else if (curr_path == 7 && getPosition().getX() >= 16) {
+	else if (curr_path == 7 && getPosition().getX() >= 14.5) {
 		setSprite("mouse-up");
 		setVelocity({0, -1 });
 		curr_path++;
 	}
 
-	else if (curr_path == 8 && getPosition().getY() <= 10) {
+	else if (curr_path == 8 && getPosition().getY() <= 9) {
 		setSprite("mouse-right");
 		setVelocity({ 1, 0 });
 		curr_path++;
 	}
 
-	else if (curr_path == 9 && getPosition().getX() >= 33) {
+	else if (curr_path == 9 && getPosition().getX() >= 32.5) {
 		setSprite("mouse-down");
 		setVelocity({0, 1});
 		curr_path++;
--- DFRemake/Path.cpp
+++ DFRemake/Path.cpp
@@ -7,6 +7,8 @@ Path::Path() {
 	setSprite("path");
 	setSolidness(SPECTRAL);
 	setAltitude(0);
-
-	setPosition({40, 9});
+	setAltitude(0);
+	float horiz = DM.getHorizontal() / 2;
+	float vert  = DM.getVertical() / 2 - 3;
+	setPosition({horiz, vert});
 }
